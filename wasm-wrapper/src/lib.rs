3e683a305f9e57e1658c4cc0785b02d35f8bb92f
sarawut <sanit.sa@outlook.com>
2020-08-06 09:46:26 +1000

    working wasm poc for powersoftau new

--- wasm-wrapper/src/lib.rs
+++ wasm-wrapper/src/lib.rs
@@ -1,12 +1,16 @@
+#[macro_use]
+extern crate serde_derive;
+
 mod utils;
 
-use powersoftau::{cli_common::*, parameters::CeremonyParams};
+use powersoftau::{cli_common::*, parameters::CeremonyParams, BatchedAccumulator};
+use snark_utils::{blank_hash, calculate_hash, UseCompression};
 
 use zexe_algebra::{Bls12_377, Bls12_381, PairingEngine as Engine, BW6_761};
 
 use wasm_bindgen::prelude::*;
 
-type Error = String;
+const COMPRESS_NEW_CHALLENGE: UseCompression = UseCompression::No;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -14,51 +18,56 @@ type Error = String;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[derive(Serialize)]
+pub struct ChallengeInfo {
+    challenge: Vec<u8>,
+    hash: Vec<u8>,
+}
+
 #[wasm_bindgen]
 pub struct Powersoftau {}
 
-#[wasm_bindgen]
+#[wasm_bindgen(catch)]
 impl Powersoftau {
-    pub fn new(curve_kind: &str, batch_size: usize, power: usize) {
-        match curve_from_str(curve_kind) {
-            Ok(curve) => {
-                // match curve {
-                //     CurveKind::Bls12_381 => new_challenge(&get_parameters::<Bls12_381>(batch_size, power)),
-                //     CurveKind::Bls12_377 => new_challenge(&get_parameters::<Bls12_377>(batch_size, power)),
-                //     CurveKind::BW6 => new_challenge(&get_parameters::<BW6_761>(batch_size, power)),
-                // };
-            }
-            Err(err) => {}
-        }
+    pub fn new(curve_kind: &str, batch_size: usize, power: usize) -> Result<JsValue, JsValue> {
+        let mut res = match curve_from_str(curve_kind).expect("123") {
+            CurveKind::Bls12_381 => new_challenge(&get_parameters::<Bls12_381>(batch_size, power)),
+            CurveKind::Bls12_377 => new_challenge(&get_parameters::<Bls12_377>(batch_size, power)),
+            CurveKind::BW6 => new_challenge(&get_parameters::<BW6_761>(batch_size, power)),
+        };
+        return Ok(JsValue::from_serde(&res.ok().unwrap()).unwrap());
     }
 }
 
-// pub fn get_parameters<E: Engine>(batch_size: usize, power: usize) -> CeremonyParams<E> {
-//     CeremonyParams::<E>::new(power, batch_size)
-// }
+pub fn get_parameters<E: Engine>(batch_size: usize, power: usize) -> CeremonyParams<E> {
+    CeremonyParams::<E>::new(power, batch_size)
+}
 
-// pub fn new_challenge<T: Engine + Sync>(parameters: &CeremonyParams<T>) {
-// Write a blank BLAKE2b hash:
-// let hash = blank_hash();
-// (&mut writable_map[0..])
-//     .write_all(hash.as_slice())
-//     .expect("unable to write a default hash to mmap");
-// writable_map
-//     .flush()
-//     .expect("unable to write blank hash to challenge file");
+pub fn new_challenge<T: Engine + Sync>(parameters: &CeremonyParams<T>) -> Result<ChallengeInfo, String> {
+    let expected_challenge_length = match COMPRESS_NEW_CHALLENGE {
+        UseCompression::Yes => parameters.contribution_size - parameters.public_key_size,
+        UseCompression::No => parameters.accumulator_size,
+    };
 
-// println!("Blank hash for an empty challenge:");
-// print_hash(&hash);
+    let mut writable_map: Vec<u8> = vec![];
+    // Write a blank BLAKE2b hash:
+    let hash = blank_hash();
 
-// BatchedAccumulator::generate_initial(&mut writable_map, COMPRESS_NEW_CHALLENGE, &parameters)
-//     .expect("generation of initial accumulator is successful");
-// writable_map.flush().expect("unable to flush memmap to disk");
+    for i in 0..expected_challenge_length {
+        writable_map.push(hash[i % hash.len()]);
+    }
 
-// // Get the hash of the contribution, so the user can compare later
-// let output_readonly = writable_map.make_read_only().expect("must make a map readonly");
-// let contribution_hash = calculate_hash(&output_readonly);
-
-// println!("Empty contribution is formed with a hash:");
-// print_hash(&contribution_hash);
-// println!("Wrote a fresh accumulator to challenge file");
-// }
+    match BatchedAccumulator::generate_initial(&mut writable_map, COMPRESS_NEW_CHALLENGE, &parameters) {
+        Ok(_) => {
+            // Get the hash of the contribution, so the user can compare later
+            let contribution_hash = calculate_hash(&writable_map);
+            return Ok(ChallengeInfo {
+                challenge: writable_map,
+                hash: contribution_hash.as_slice().iter().cloned().collect(),
+            });
+        }
+        Err(_) => {
+            return Err("generation of initial accumulator is successful".to_string());
+        }
+    };
+}
