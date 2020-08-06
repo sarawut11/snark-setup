db6dd503fd2e445273f7a9c1e6d2a1acb22849ce
sarawut <sanit.sa@outlook.com>
2020-08-07 00:59:43 +1000

    remove new, implement contribute of powersoftau

--- wasm-wrapper/src/lib.rs
+++ wasm-wrapper/src/lib.rs
@@ -1,16 +1,23 @@
 #[macro_use]
 extern crate serde_derive;
 
-mod utils;
+use rand::Rng;
 
-use powersoftau::{cli_common::*, parameters::CeremonyParams, BatchedAccumulator};
-use snark_utils::{blank_hash, calculate_hash, UseCompression};
+use powersoftau::{
+    cli_common::*,
+    keypair::keypair,
+    parameters::{CeremonyParams, CheckForCorrectness},
+    BatchedAccumulator,
+};
+use snark_utils::{calculate_hash, get_rng, user_system_randomness, UseCompression};
 
 use zexe_algebra::{Bls12_377, Bls12_381, PairingEngine as Engine, BW6_761};
 
 use wasm_bindgen::prelude::*;
 
-const COMPRESS_NEW_CHALLENGE: UseCompression = UseCompression::No;
+const INPUT_IS_COMPRESSED: UseCompression = UseCompression::No;
+const COMPRESS_THE_OUTPUT: UseCompression = UseCompression::Yes;
+const CHECK_INPUT_CORRECTNESS: CheckForCorrectness = CheckForCorrectness::No;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -19,21 +26,27 @@ const COMPRESS_NEW_CHALLENGE: UseCompression = UseCompression::No;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[derive(Serialize)]
-pub struct ChallengeInfo {
-    challenge: Vec<u8>,
-    hash: Vec<u8>,
+pub struct ContributionResponse {
+    current_accumulator_hash: Vec<u8>,
+    response: Vec<u8>,
+    contribution_hash: Vec<u8>,
 }
 
 #[wasm_bindgen]
 pub struct Powersoftau {}
 
-#[wasm_bindgen(catch)]
+#[wasm_bindgen]
 impl Powersoftau {
-    pub fn new(curve_kind: &str, batch_size: usize, power: usize) -> Result<JsValue, JsValue> {
-        let mut res = match curve_from_str(curve_kind).expect("123") {
-            CurveKind::Bls12_381 => new_challenge(&get_parameters::<Bls12_381>(batch_size, power)),
-            CurveKind::Bls12_377 => new_challenge(&get_parameters::<Bls12_377>(batch_size, power)),
-            CurveKind::BW6 => new_challenge(&get_parameters::<BW6_761>(batch_size, power)),
+    pub fn contribute(curve_kind: &str, batch_size: usize, power: usize, challenge: &[u8]) -> Result<JsValue, JsValue> {
+        let rng = get_rng(&user_system_randomness());
+        let res = match curve_from_str(curve_kind).expect("invalid curve_kind") {
+            CurveKind::Bls12_381 => {
+                contribute_challenge(&challenge, &get_parameters::<Bls12_381>(batch_size, power), rng)
+            }
+            CurveKind::Bls12_377 => {
+                contribute_challenge(&challenge, &get_parameters::<Bls12_377>(batch_size, power), rng)
+            }
+            CurveKind::BW6 => contribute_challenge(&challenge, &get_parameters::<BW6_761>(batch_size, power), rng),
         };
         return Ok(JsValue::from_serde(&res.ok().unwrap()).unwrap());
     }
@@ -43,31 +56,68 @@ pub fn get_parameters<E: Engine>(batch_size: usize, power: usize) -> CeremonyPar
     CeremonyParams::<E>::new(power, batch_size)
 }
 
-pub fn new_challenge<T: Engine + Sync>(parameters: &CeremonyParams<T>) -> Result<ChallengeInfo, String> {
-    let expected_challenge_length = match COMPRESS_NEW_CHALLENGE {
-        UseCompression::Yes => parameters.contribution_size - parameters.public_key_size,
+pub fn contribute_challenge<T: Engine + Sync>(
+    challenge: &[u8],
+    parameters: &CeremonyParams<T>,
+    mut rng: impl Rng,
+) -> Result<ContributionResponse, String> {
+    let expected_challenge_length = match INPUT_IS_COMPRESSED {
+        UseCompression::Yes => parameters.contribution_size,
         UseCompression::No => parameters.accumulator_size,
     };
 
-    let mut writable_map: Vec<u8> = vec![];
-    // Write a blank BLAKE2b hash:
-    let hash = blank_hash();
-
-    for i in 0..expected_challenge_length {
-        writable_map.push(hash[i % hash.len()]);
+    if challenge.len() != expected_challenge_length {
+        return Err(format!(
+            "The size of challenge file should be {}, but it's {}, so something isn't right.",
+            expected_challenge_length,
+            challenge.len()
+        ));
     }
 
-    match BatchedAccumulator::generate_initial(&mut writable_map, COMPRESS_NEW_CHALLENGE, &parameters) {
-        Ok(_) => {
-            // Get the hash of the contribution, so the user can compare later
-            let contribution_hash = calculate_hash(&writable_map);
-            return Ok(ChallengeInfo {
-                challenge: writable_map,
-                hash: contribution_hash.as_slice().iter().cloned().collect(),
-            });
-        }
-        Err(_) => {
-            return Err("generation of initial accumulator is successful".to_string());
-        }
+    let required_output_length = match COMPRESS_THE_OUTPUT {
+        UseCompression::Yes => parameters.contribution_size,
+        UseCompression::No => parameters.accumulator_size + parameters.public_key_size,
     };
+
+    let mut response: Vec<u8> = vec![];
+    let current_accumulator_hash = calculate_hash(&challenge);
+
+    for i in 0..required_output_length {
+        response.push(current_accumulator_hash[i % current_accumulator_hash.len()]);
+    }
+
+    // Construct our keypair using the RNG we created above
+    let (pubkey, privkey) = match keypair(&mut rng, current_accumulator_hash.as_ref()) {
+        Ok(pair) => pair,
+        Err(_) => return Err("could not generate keypair".to_string()),
+    };
+
+    // this computes a transformation and writes it
+    match BatchedAccumulator::contribute(
+        &challenge,
+        &mut response,
+        INPUT_IS_COMPRESSED,
+        COMPRESS_THE_OUTPUT,
+        CHECK_INPUT_CORRECTNESS,
+        &privkey,
+        &parameters,
+    ) {
+        Ok(_) => match pubkey.write(&mut response, COMPRESS_THE_OUTPUT, &parameters) {
+            Ok(_) => {
+                let contribution_hash = calculate_hash(&response);
+
+                return Ok(ContributionResponse {
+                    current_accumulator_hash: current_accumulator_hash.as_slice().iter().cloned().collect(),
+                    response,
+                    contribution_hash: contribution_hash.as_slice().iter().cloned().collect(),
+                });
+            }
+            Err(_) => {
+                return Err("unable to write public key".to_string());
+            }
+        },
+        Err(_) => {
+            return Err("must contribute with the key".to_string());
+        }
+    }
 }
